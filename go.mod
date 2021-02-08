a7dde4b527d0b1cf5c0111ee0ccf376a0a0fa2d7
Jean-Felix <jeanfelix.majurel@gmail.com>
2021-02-08 20:04:57 +0100

    +Fixed issue on mail sender

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module mailerapi
 go 1.15
 
 require (
+	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gorilla/mux v1.8.0
 	github.com/microcosm-cc/bluemonday v1.0.4
 )
--- main.go
+++ main.go
@@ -1,40 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"io/ioutil"
-	"net/http"
-	"os"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/smtp"
+	"os"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"github.com/microcosm-cc/bluemonday"
 )
 
 type Message struct {
-    From      string `json:"from"`
-    Message   string `json:"message"`
+	From    string `json:"from" validate:"required,email"`
+	Message string `json:"message" validate:"required"`
 }
 
-var senderEmailAccount string;
-var receiverEmailAccount string;
-var smtpServerUrl string;
-var smtpSecretPass string;
+var senderEmailAccount string
+var receiverEmailAccount string
+var smtpServerURL string
+var smtpSecretPass string
 
+var validate *validator.Validate
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
 
 func mailHandler(w http.ResponseWriter, r *http.Request) {
-	
-    reqBody, _ := ioutil.ReadAll(r.Body) //read request body
 
-	reqBody = bluemonday.UGCPolicy().SanitizeBytes(reqBody) //sanitize reqBody
+	reqBody, _ := ioutil.ReadAll(r.Body) //read request body
 
-	var message Message 
-    json.Unmarshal(reqBody, &message); //convert to a message type
-	sendEmail(receiverEmailAccount, "Contact from " + message.From , message.Message)
+	sanitizer := bluemonday.UGCPolicy()
+
+	var message Message
+	json.Unmarshal(reqBody, &message) //convert to a message type
+	err := validate.Struct(message)
+
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	message.From, message.Message = sanitizer.Sanitize(message.From), sanitizer.Sanitize(message.Message)
+	err = sendEmail(receiverEmailAccount, "Contact from "+message.From, message.Message)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 }
 
 func handleRequest(port string) {
@@ -42,17 +59,37 @@ func handleRequest(port string) {
 
 	myRouter.HandleFunc("/", homeHandler).Methods("GET")
 	myRouter.HandleFunc("/", mailHandler).Methods("POST")
-	
-	if port == "" { port = ":8080" }
-	log.Fatal(http.ListenAndServe(port, myRouter))
+
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, myRouter))
+}
+
+func sendEmail(sendTo string, subject string, msg string) error {
+
+	var emailBody []byte
+	auth := smtp.PlainAuth("", senderEmailAccount, smtpSecretPass, smtpServerURL)
+
+	to := []string{sendTo}
+	emailBody = []byte("To: " + sendTo + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"\r\n" +
+		msg + "\r\n")
+
+	err := smtp.SendMail(smtpServerURL+":587", auth, senderEmailAccount, to, emailBody)
+
+	return err
 }
 
 func main() {
 	port := os.Getenv("PORT")
 	senderEmailAccount = os.Getenv("SENDER_EMAIL_ACCOUNT")
 	receiverEmailAccount = os.Getenv("RECEIVER_EMAIL_ACCOUNT")
-	smtpServerUrl = os.Getenv("SMTP_SERVER_URL")
+	smtpServerURL = os.Getenv("SMTP_SERVER_URL")
 	smtpSecretPass = os.Getenv("SMTP_SECRET_PASS")
+
+	validate = validator.New()
 
 	handleRequest(port)
 }
